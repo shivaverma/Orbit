e5c36fe244501b61c2341e307018190adc12b581
shiva verma <shivajbd@gmail.com>
2019-11-19 09:15:15 +0530

    Created Paddle Environment

---
+++ paddle/agent.py
@@ -0,0 +1,108 @@
+from paddle import Paddle
+
+import random
+import numpy as np
+from keras import Sequential
+from collections import deque
+from keras.layers import Dense
+import matplotlib.pyplot as plt
+from keras.optimizers import adam
+
+env = Paddle()
+np.random.seed(0)
+
+
+class DQN:
+
+    """ Implementation of deep q learning algorithm """
+
+    def __init__(self, action_space, state_space):
+
+        self.action_space = action_space
+        self.state_space = state_space
+        self.epsilon = 1
+        self.gamma = .95
+        self.batch_size = 64
+        self.epsilon_min = .01
+        self.epsilon_decay = .995
+        self.learning_rate = 0.001
+        self.memory = deque(maxlen=100000)
+        self.model = self.build_model()
+
+    def build_model(self):
+
+        model = Sequential()
+        model.add(Dense(64, input_shape=(self.state_space,), activation='relu'))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(self.action_space, activation='linear'))
+        model.compile(loss='mse', optimizer=adam(lr=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_space)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
+
+    def replay(self):
+
+        if len(self.memory) < self.batch_size:
+            return
+
+        minibatch = random.sample(self.memory, self.batch_size)
+        states = np.array([i[0] for i in minibatch])
+        actions = np.array([i[1] for i in minibatch])
+        rewards = np.array([i[2] for i in minibatch])
+        next_states = np.array([i[3] for i in minibatch])
+        dones = np.array([i[4] for i in minibatch])
+
+        states = np.squeeze(states)
+        next_states = np.squeeze(next_states)
+
+        targets = rewards + self.gamma*(np.amax(self.model.predict_on_batch(next_states), axis=1))*(1-dones)
+        targets_full = self.model.predict_on_batch(states)
+
+        ind = np.array([i for i in range(self.batch_size)])
+        targets_full[[ind], [actions]] = targets
+
+        self.model.fit(states, targets_full, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+
+def train_dqn(episode):
+
+    loss = []
+    agent = DQN(3, 5)
+    for e in range(episode):
+        state = env.reset()
+        state = np.reshape(state, (1, 5))
+        score = 0
+        max_steps = 1000
+        for i in range(max_steps):
+            action = agent.act(state)
+            reward, next_state, done = env.step(action)
+            score += reward
+            next_state = np.reshape(next_state, (1, 5))
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            agent.replay()
+            if done:
+                print("episode: {}/{}, score: {}".format(e, episode, score))
+                break
+        loss.append(score)
+    return loss
+
+
+if __name__ == '__main__':
+
+    ep = 100
+    loss = train_dqn(ep)
+    plt.plot([i for i in range(ep)], loss)
+    plt.xlabel('episodes')
+    plt.ylabel('reward')
+    plt.show()
---
+++ paddle/paddle.py
@@ -0,0 +1,147 @@
+
+import turtle as t
+
+
+class Paddle():
+
+    def __init__(self):
+
+        self.done = False
+        self.reward = 0
+        self.hit, self.miss = 0, 0
+
+        # Setup Background
+
+        self.win = t.Screen()
+        self.win.title('Paddle')
+        self.win.bgcolor('black')
+        self.win.setup(width=600, height=600)
+        self.win.tracer(0)
+
+        # Paddle
+
+        self.paddle = t.Turtle()
+        self.paddle.speed(0)
+        self.paddle.shape('square')
+        self.paddle.shapesize(stretch_wid=1, stretch_len=5)
+        self.paddle.color('white')
+        self.paddle.penup()
+        self.paddle.goto(0, -275)
+
+        # Ball
+
+        self.ball = t.Turtle()
+        self.ball.speed(0)
+        self.ball.shape('circle')
+        self.ball.color('red')
+        self.ball.penup()
+        self.ball.goto(0, 100)
+        self.ball.dx = 3
+        self.ball.dy = -3
+
+        # Score
+
+        self.score = t.Turtle()
+        self.score.speed(0)
+        self.score.color('white')
+        self.score.penup()
+        self.score.hideturtle()
+        self.score.goto(0, 250)
+        self.score.write("Hit: {}   Missed: {}".format(self.hit, self.miss), align='center', font=('Courier', 24, 'normal'))
+
+        # -------------------- Keyboard control ----------------------
+
+        self.win.listen()
+        self.win.onkey(self.paddle_right, 'Right')
+        self.win.onkey(self.paddle_left, 'Left')
+
+    # Paddle movement
+
+    def paddle_right(self):
+
+        x = self.paddle.xcor()
+        if x < 225:
+            self.paddle.setx(x+20)
+
+    def paddle_left(self):
+
+        x = self.paddle.xcor()
+        if x > -225:
+            self.paddle.setx(x-20)
+
+    # ------------------------ AI control ------------------------
+
+    # 0 move left
+    # 1 do nothing
+    # 2 move right
+
+    def reset(self):
+
+        self.paddle.goto(0, -275)
+        self.ball.goto(0, 100)
+        return [self.paddle.xcor()*0.01, self.ball.xcor()*0.01, self.ball.ycor()*0.01, self.ball.dx, self.ball.dy]
+
+    def step(self, action):
+
+        self.reward = 0
+        self.done = 0
+
+        if action == 0:
+            self.paddle_left()
+            self.reward -= .1
+
+        if action == 2:
+            self.paddle_right()
+            self.reward -= .1
+
+        self.run_frame()
+
+        state = [self.paddle.xcor()*0.01, self.ball.xcor()*0.01, self.ball.ycor()*0.01, self.ball.dx, self.ball.dy]
+        return self.reward, state, self.done
+
+    def run_frame(self):
+
+        self.win.update()
+
+        # Ball moving
+
+        self.ball.setx(self.ball.xcor() + self.ball.dx)
+        self.ball.sety(self.ball.ycor() + self.ball.dy)
+
+        # Ball and Wall collision
+
+        if self.ball.xcor() > 290:
+            self.ball.setx(290)
+            self.ball.dx *= -1
+
+        if self.ball.xcor() < -290:
+            self.ball.setx(-290)
+            self.ball.dx *= -1
+
+        if self.ball.ycor() > 290:
+            self.ball.sety(290)
+            self.ball.dy *= -1
+
+        # Ball Ground contact
+
+        if self.ball.ycor() < -290:
+            self.ball.goto(0, 100)
+            self.miss += 1
+            self.score.clear()
+            self.score.write("Hit: {}   Missed: {}".format(self.hit, self.miss), align='center', font=('Courier', 24, 'normal'))
+            self.reward -= 3
+            self.done = True
+
+        # Ball Paddle collision
+
+        if abs(self.ball.ycor() + 250) < 2 and abs(self.paddle.xcor() - self.ball.xcor()) < 55:
+            self.ball.dy *= -1
+            self.hit += 1
+            self.score.clear()
+            self.score.write("Hit: {}   Missed: {}".format(self.hit, self.miss), align='center', font=('Courier', 24, 'normal'))
+            self.reward += 3
+
+
+# while True:
+#
+#      env.run_frame()
