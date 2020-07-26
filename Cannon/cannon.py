987d6210148b11ab82644da9d2fb2ae161ae7b10
sathish kumar <38582321+sathishk0230@users.noreply.github.com>
2020-07-26 20:00:55 +0530

    working RL agent

--- Cannon/cannon.py
+++ Cannon/cannon.py
@@ -5,14 +5,9 @@ import sys
 import os
 from random import randint
 from pygame.locals import QUIT,KEYDOWN,K_ESCAPE,K_LEFT,K_RIGHT,K_SPACE,K_a,K_d
-from math import sin, cos, pi
+from math import sin, cos, pi , inf
 
-WIN_WIDTH = 600
-WIN_HEIGHT = 600
-FPS = 30
-pygame.font.init()
-pygame.init()
-STAT_FONT = pygame.font.SysFont("comicsans", 30)
+LIVES = 5
 
 
 def rotate(surface, angle, pivot, offset):
@@ -36,7 +31,9 @@ class Plank:
     PLANK_WIDTH = 150
     PLANK_HEIGHT = 30
 
-    def __init__(self):
+    def __init__(self,WIN_WIDTH,WIN_HEIGHT):
+        self.WIN_WIDTH = WIN_WIDTH
+        self.WIN_HEIGHT = WIN_HEIGHT
         self.xvel = 1
         self.rect = pygame.Rect(0, 0, self.PLANK_WIDTH, self.PLANK_HEIGHT)
         self.rect.center = (WIN_WIDTH/2, WIN_HEIGHT-50)
@@ -46,8 +43,11 @@ class Plank:
         )
 
     def move(self):
-        if self.rect.right >= WIN_WIDTH or self.rect.left <= 0:
-            self.xvel = -self.xvel
+        if self.rect.right >= self.WIN_WIDTH or self.rect.left <= 0:
+            if abs(self.xvel)<5:
+                self.xvel = -1.3*self.xvel
+            else:
+                self.xvel = -self.xvel
         self.rect.left += self.xvel
 
     def draw(self, win, angle):
@@ -63,12 +63,13 @@ class Ball:
     MAX_RADIUS = 25
     MIN_RADIUS = 5
 
-    def __init__(self):
+    def __init__(self,WIN_WIDTH,max_y):
+        self.WIN_WIDTH = WIN_WIDTH
         self.radius = 25
         self.center = [
-            randint(self.MAX_RADIUS, WIN_WIDTH-self.MAX_RADIUS), randint(-70, -55)]
+            randint(self.MAX_RADIUS, WIN_WIDTH-self.MAX_RADIUS), randint(max_y-100, max_y-50)]
         self.surf = pygame.Surface((50, 50), pygame.SRCALPHA)
-        pygame.draw.circle(self.surf, (25, 224, 71), (25, 25), self.radius)
+        pygame.draw.circle(self.surf, (255,0,0), (25, 25), self.radius)
         self.mask = pygame.mask.from_surface(self.surf)
         self.rect = self.surf.get_rect(center=self.center)
         self.yvel = 5
@@ -84,17 +85,18 @@ class Ball:
 class Bullet:
     VEL = 10
 
-    def __init__(self, x, y, angle, plank_xvel):
+    def __init__(self, x, y, angle,WIN_WIDTH):
+        self.WIN_WIDTH = WIN_WIDTH
         self.surf = pygame.Surface((20, 20), pygame.SRCALPHA)
         pygame.draw.circle(self.surf, (247, 136, 25), (10, 10), 10)
         self.mask = pygame.mask.from_surface(self.surf)
         x, y = x+50*sin(pi*(angle/180)), y-50*cos(pi*(angle/180))
         self.rect = self.surf.get_rect(center=(x, y))
-        self.dx = self.VEL*sin(pi*(angle/180))+plank_xvel
+        self.dx = self.VEL*sin(pi*(angle/180))
         self.dy = -self.VEL*cos(pi*(angle/180))
 
     def move(self):
-        if self.rect.left <= 0 or self.rect.right >= WIN_WIDTH:
+        if self.rect.left <= 0 or self.rect.right >= self.WIN_WIDTH:
             self.dx = -self.dx
         self.rect.move_ip(self.dx, self.dy)
 
@@ -103,38 +105,57 @@ class Bullet:
         win.blit(self.surf, self.rect)
 
 
-def draw_frame(win, plank, circles, bullets, angle, score, lives):
-    win.fill((0, 0, 0))
-    pygame.draw.line(win, (100, 100, 100), (0, WIN_HEIGHT-150), (WIN_WIDTH, WIN_HEIGHT-150))
-    for circle in circles:
-        circle.draw(win)
-    for bullet in bullets:
-        bullet.draw(win)
-    plank.draw(win, angle)
-
-    score_label = STAT_FONT.render("Score: " + str(score), 1, (255, 255, 255))
-    win.blit(score_label, (WIN_WIDTH - score_label.get_width() - 15, 10))
-    lives_label = STAT_FONT.render("Lives: " + str(lives), 1, (255, 255, 255))
-    win.blit(lives_label, (15, 10))
 
 
-if __name__ == '__main__':
+class Cannon:
+    WIN_WIDTH = 600
+    WIN_HEIGHT = 600
+    FPS = 30
+    # ADDBALL = pygame.USEREVENT + 1
+    def __init__(self):
+        pygame.font.init()
+        pygame.init()
+        self.STAT_FONT = pygame.font.SysFont("comicsans", 30)
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Cannon")
+        self.WIN = pygame.display.set_mode((self.WIN_WIDTH, self.WIN_HEIGHT))
+        self.last_ball = 0
 
-    clock = pygame.time.Clock()
-    pygame.display.set_caption("Cannon")
-    WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-    plank = Plank()
-    angle = 0
-    lives = 3
-    score = 0
-    ADDBALL = pygame.USEREVENT + 1
-    pygame.time.set_timer(ADDBALL, 2000)
-    balls = [Ball()]
-    bullets = list()
-    shoot_counter = 5
 
-    while True:
-        clock.tick(FPS)
+        self.plank = Plank(self.WIN_WIDTH,self.WIN_HEIGHT)
+        self.angle = 0
+        self.lives = LIVES
+        self.score = 0
+        self.balls = list()
+        for i in range(3):
+            b = Ball(self.WIN_WIDTH,self.last_ball)
+            self.balls.append(b)
+            self.last_ball = b.center[1]
+
+        self.bullets = list()
+        self.shoot_counter = 5
+        # self.ball_counter = 60
+
+
+
+    def draw_frame(self):
+        self.WIN.fill((0, 0, 0))
+        pygame.draw.line(self.WIN, (100, 100, 100), (0, self.WIN_HEIGHT-150), (self.WIN_WIDTH, self.WIN_HEIGHT-150))
+        for ball in self.balls:
+            ball.draw(self.WIN)
+        for bullet in self.bullets:
+            bullet.draw(self.WIN)
+        self.plank.draw(self.WIN, self.angle)
+
+        score_label = self.STAT_FONT.render("Score: " + str(self.score), 1, (255, 255, 255))
+        self.WIN.blit(score_label, (self.WIN_WIDTH - score_label.get_width() - 15, 10))
+        lives_label = self.STAT_FONT.render("Lives: " + str(self.lives), 1, (255, 255, 255))
+        self.WIN.blit(lives_label, (15, 10))
+
+    def game_loop(self):
+        hit = False
+        life_down = False
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -143,48 +164,161 @@ if __name__ == '__main__':
                 if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-            elif event.type == ADDBALL:
-                balls.append(Ball())
-        for i, bullet in enumerate(bullets):
-            for j, ball in enumerate(balls):
+            # elif event.type == self.ADDBALL:
+                # self.balls.append(Ball(self.WIN_WIDTH))
+        # self.ball_counter-=1
+        
+        # Detect collision
+        for i, bullet in enumerate(self.bullets):
+            for j, ball in enumerate(self.balls):
                 offset_x = bullet.rect.x - ball.rect.x
                 offset_y = bullet.rect.y - ball.rect.y
                 overlap = ball.mask.overlap(bullet.mask, (offset_x, offset_y))
                 if overlap:
-                    balls.pop(j)
-                    bullets.pop(i)
+                    self.balls.pop(j)
+                    self.bullets.pop(i)
                     del ball
                     del bullet
-                    score += 1
+                    self.score += 1
+                    hit = True
                     break
-        for index, ball in enumerate(balls):
-            if ball.rect.center[1]+ball.radius >= WIN_HEIGHT-150:
-                balls.pop(index)
+        # lost a life
+        for index, ball in enumerate(self.balls):
+            if ball.rect.center[1]+ball.radius >= self.WIN_HEIGHT-150:
+                self.balls.pop(index)
                 del ball
-                lives -= 1
+                self.lives -= 1
+                life_down = True
                 break
-        for index, bullet in enumerate(bullets):
+        # delete bullets out of the screen
+        for index, bullet in enumerate(self.bullets):
             if bullet.rect.bottom <= 0:
-                bullets.pop(index)
+                self.bullets.pop(index)
                 del bullet
                 break
+        while len(self.balls)<3:
+            b = Ball(self.WIN_WIDTH,self.last_ball)
+            self.balls.append(b)
+            self.last_ball = b.center[1]
+
+        self.shoot_counter -= 1
+        return hit,life_down
+
+    def play(self):
+        while True:
+            self.clock.tick(self.FPS)
+            
+            hit, life_down = self.game_loop()
+            
+            # input from user
+            keys = pygame.key.get_pressed()
+            if (keys[K_d] or keys[pygame.K_RIGHT]) and self.angle < 70:
+                self.angle += 3
+            elif (keys[K_a] or keys[pygame.K_LEFT]) and self.angle > -70:
+                self.angle -= 3
+            if self.shoot_counter < 0 and keys[pygame.K_SPACE]:
+                self.shoot_counter = 5
+                x, y = self.plank.rect.center
+                self.bullets.append(Bullet(x, y, self.angle,self.WIN_WIDTH))
+            
+            # game termination condition 
+            if self.lives < 0:
+                pygame.quit()
+                sys.exit()
+
+            self.draw_frame()
+            pygame.display.update()
+        pygame.quit()
+
+    def reset(self):
+        self.plank.__init__(self.WIN_WIDTH,self.WIN_HEIGHT)
+        self.angle = 0
+        self.lives = LIVES
+        self.score = 0
+        self.balls = list()
+        self.last_ball = 0
+        for i in range(3):
+            b = Ball(self.WIN_WIDTH,self.last_ball)
+            self.balls.append(b)
+            self.last_ball = b.center[1]
+
+        self.bullets = list()
+        self.shoot_counter = 5
+        # self.ball_counter = 60
+        
+        ball_cor = list()
+        for ball in self.balls:
+            ball_cor.append(ball.rect.center[0]/self.WIN_WIDTH)
+            ball_cor.append(ball.rect.center[1]/self.WIN_HEIGHT)
+        state = [
+        self.plank.rect.center[0]/self.WIN_WIDTH,
+        sin(self.angle),
+        *ball_cor
+        ]
+        
+        
+        return state
+
+    def step(self,action):      
+
+        self.reward = 0
+        self.done = 0
+
+        # left
+        if action == 0:
+            self.reward -=.1
+            if self.angle > -70:
+                self.angle-=3
+            
+        # right
+        elif action == 1:
+            self.reward -=.1
+            if self.angle < 70:
+                self.angle+=3
+            
+        # shoot
+        elif action == 2:
+            self.reward -=.5
+            if self.shoot_counter < 0:
+                self.shoot_counter = 5
+                x, y = self.plank.rect.center
+                self.bullets.append(Bullet(x, y, self.angle,self.WIN_WIDTH))
+        
+        if self.lives < 0 :
+            self.reset()
+
+        hit, life_down = self.game_loop()
+
+        if hit:
+            self.reward += 5
+        if life_down:
+            self.reward -= 5
+        if self.lives<0:
+            self.done = True
+
+        ball_cor = list()
+        for ball in self.balls:
+            ball_cor.append(ball.rect.center[0]/self.WIN_WIDTH)
+            ball_cor.append(ball.rect.center[1]/self.WIN_HEIGHT)
+        state = [
+        self.plank.rect.center[0]/self.WIN_WIDTH,
+        sin(self.angle),
+        *ball_cor
+        ]
+        
         
 
-        if lives < 0:
-            pygame.quit()
-            sys.exit()
-
-        keys = pygame.key.get_pressed()
-        if (keys[K_d] or keys[pygame.K_RIGHT]) and angle < 80:
-            angle += 3
-        elif (keys[K_a] or keys[pygame.K_LEFT]) and angle > -80:
-            angle -= 3
-        if shoot_counter < 0 and keys[pygame.K_SPACE]:
-            shoot_counter = 5
-            x, y = plank.rect.center
-            bullets.append(Bullet(x, y, angle, plank.xvel))
-        shoot_counter -= 1
-
-        draw_frame(WIN, plank, balls, bullets, angle, score, lives)
+######## remove this later #########################################################
+        self.draw_frame()
         pygame.display.update()
-    pygame.quit()
+        # self.clock.tick(self.FPS)
+
+
+        return self.reward , state , self.done
+
+                    
+
+
+if __name__ == '__main__':
+    env = Cannon()
+    env.play()
